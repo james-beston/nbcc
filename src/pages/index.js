3717b2bcee6b05767cd5af7fc5de951d0bc4a391
James Beston <james@beston.me>
2022-05-08 16:42:18 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import * as React from "react"
 // markup
 const IndexPage = () => {
   return (
-    <main styleName="max-w-screen-7xl mx-auto">
-      <title>Home Page</title>
-      <h1 styleName="font-4xl text-gray-800">
-        Hello World
+    <main className="mx-auto max-w-screen-xl">
+      <title>NBCC</title>
+      <h1 className="text-9xl text-gray-700">
+        NBCC coming soon!
       </h1>
     </main>
   )
